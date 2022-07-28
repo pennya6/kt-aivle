5051c28510df6007d8e45f06602b4a2552da8ea6
pennya6 <pennya6@naver.com>
2022-07-28 11:56:40 +0900

    미니실습3

--- 미니실습3.py
+++ 미니실습3.py
@@ -1,13 +1,9 @@
-from re import A
-
-
 a=int(input("정수 a:"))
 b=int(input("정수 b:"))
 if a>b:
     a,b=b,a
 sum=0
 for i in range(a,b+1):
-    if i<b+1:
-        print(f'+{i}',end="")
+    print(f'+{i}',end="")
     sum+=i
 print(f'={sum}')
