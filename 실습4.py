9e4543125fa8302ddc2a496ed4f6bdca8313390f
pennya6 <pennya6@naver.com>
2022-07-28 14:04:40 +0900

    실습 4 범위 재설정

--- 실습4.py
+++ 실습4.py
@@ -1,6 +1,6 @@
 n=int(input('정수를 입력하세오 : '))
-for i in range(1,10):
-    for j in range(1,7):
+for i in range(1,n):
+    for j in range(2,7):
         if i**j==n:
             print(f'{i}**{j}')
         
