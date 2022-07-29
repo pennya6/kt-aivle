6df2084c91c4d287d4c17d1e9900c6725d0e32c7
pennya6 <pennya6@naver.com>
2022-07-29 12:27:49 +0900

    자료구조 실습1

--- python/자료구조_실습1.py
+++ python/자료구조_실습1.py
@@ -1,10 +1,11 @@
 a=[]
 b=True
 for i in range(2,1001):
-    for j in range(1,i+1):
+    b=True
+    for j in range(2,i):
         if i%j==0:
             b=False
-     
+            break
     if b==True:
         a.append(i)       
             
