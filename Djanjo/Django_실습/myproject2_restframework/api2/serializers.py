7594ea67d105ef2ee1b4466cbad68bc27462a03f
김시연 <pennya6@hs.ac.kr>
2022-11-18 13:53:54 +0900

    장고

--- Djanjo/Django_실습/myproject2_restframework/api2/serializers.py
+++ Djanjo/Django_실습/myproject2_restframework/api2/serializers.py
@@ -7,3 +7,29 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
+        
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Post
+        fields=['id','title','image','like','category']
+        
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Comment
+        fields='__all__'
+        
+class PostListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Post
+        fields=['id','title','image','like','category']
+
+class PostRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Post
+        exclude =['create_dt']
+        
+class PostLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Post
+        fields=['like']
+        
